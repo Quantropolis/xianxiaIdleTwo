7043914d7c9205818896cb2e725a6ea93ddcc2cc
Quantropolis <u18at19@abdn.ac.uk>
2019-09-19 10:39:27 +0100

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,5 +1,8 @@
 require 'sinatra' 					# includes gem to process web pages
 set :bind, '0.0.0.0'
+source "https://rubygems.org"
+
+gem "github-pages", group: :jekyll_plugins
 
 get '/' do
   '<p>Hello World</p>'
